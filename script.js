b1977a978a00a020b8e6182a9057f848f0376d6c
Crunk <jb10051075@gmail.com>
2021-10-29 02:50:16 +0200

    Correct id in html change length of wrong letters CSS Add update  wrong letter, display parts of hangman, check if lost, check letterkey press restart game

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+console.log(wrongLettersEl);
 
 const figureParts = document.querySelectorAll('.figure-part');
 
@@ -13,7 +14,7 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 console.log(selectedWord);
 
-const correctLetters = ['p', 'a', 's', 't', 'a'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
@@ -31,12 +32,94 @@ function displayWord() {
 			// Turn the letter back into a string from the array
 			.join('')}`;
 
+	// Fix new line character of each letter
 	const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
 
+	// Check if the words are equal and show final message pop up
 	if (innerWord === selectedWord) {
-		finalMessage.innerText = 'Helemaal Mooi dit!';
+		finalMessage.innerText = 'Helemaal Mooi dit! 😈';
 		popup.style.display = 'flex';
 	}
 }
+
+// Update wrong letters
+function updateWrongLetterEl() {
+	console.log('update wrong' + wrongLettersEl);
+
+	// Display wrong letters | map through each letter
+	wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''} 
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+    `;
+
+	// Display parts
+	figureParts.forEach((part, index) => {
+		// Check how many error are in the wrongletters
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
+
+	// Check if lost
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = `je hebt verloren`;
+		popup.style.display = 'flex';
+	}
+}
+
+// Show notification
+function showNotification() {
+	notification.classList.add('show');
+
+	setTimeout(() => {
+		notification.classList.remove('show');
+	}, 2000);
+}
+
+// keydown letter press
+window.addEventListener('keydown', (e) => {
+	// Check if only letters are typed in
+	if (e.code >= 'KeyA' && e.code <= 'KeyZ') {
+		// Check if the letter is in the selected word
+		const letter = e.key;
+		if (selectedWord.includes(letter)) {
+			// If the correct letter is in the CorrectLetters push the letter into the array and update word.
+			if (!correctLetters.includes(letter)) {
+				correctLetters.push(letter);
+				displayWord();
+			} else {
+				// Show the existing letter in notification
+				showNotification();
+			}
+		} else {
+			// If the wrong letter is typed push it into the wrongLetter array and update it
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter);
+
+				updateWrongLetterEl();
+			} else {
+				showNotification();
+			}
+		}
+	}
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+	// Empty array
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+	updateWrongLetterEl();
+
+	popup.style.display = 'none';
+});
 
 displayWord();
